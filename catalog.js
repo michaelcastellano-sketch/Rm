8bf39ac6fe3f2a828c85655e628bd9ce3f7c16fc
michaelcastellano-sketch <michael.castellano@wmtps.net>
2025-12-06 08:37:36 -0500

    Add files via upload

---
+++ catalog.js
@@ -0,0 +1,26 @@
+const catalog = [
+  {
+    "id": 1,
+    "name": "UniFi Dream Machine Pro",
+    "type": "gateway",
+    "your_price": 341.1
+  },
+  {
+    "id": 2,
+    "name": "UniFi Dream Machine",
+    "type": "gateway",
+    "your_price": 269.1
+  },
+  {
+    "id": 3,
+    "name": "UniFi USW-Flex Switch",
+    "type": "switch",
+    "your_price": 89.1
+  },
+  {
+    "id": 4,
+    "name": "UniFi UVC-G6 Camera",
+    "type": "camera",
+    "your_price": 161.1
+  }
+];
